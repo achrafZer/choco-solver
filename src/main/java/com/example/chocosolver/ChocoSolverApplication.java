93bb167f38f67b87c445015931815f731857f51e
Achraf <achraf.ZERHOUNI-ABDOU@etu.univ-amu.fr>
2023-05-03 22:56:27 +0200

    Initial Commit

---
+++ src/main/java/com/example/chocosolver/ChocoSolverApplication.java
@@ -0,0 +1,13 @@
+package com.example.chocosolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChocoSolverApplication.class, args);
+    }
+
+}
