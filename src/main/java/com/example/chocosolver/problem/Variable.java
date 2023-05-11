3d48482b17830a3f8254450b90be867bf5c210be
NICOLAS Elie <elie.nicolas@etu.univ-amu.fr>
2023-05-11 13:16:13 +0000

    Update Variable.java

--- src/main/java/com/example/chocosolver/problem/Variable.java
+++ src/main/java/com/example/chocosolver/problem/Variable.java
@@ -4,23 +4,36 @@ import org.jgrapht.alg.util.Pair;
 
 import java.util.List;
 
+@Data
 public class Variable {
 
     private String name;
+    private boolean isANumber;
+    private Pair<Integer, Integer> valueInterval;
+    private List<Integer> valueSet;
+    private Integer constantValue;
+
+
 
     public Variable(String name, Pair<Integer, Integer> valueInterval) {
         //TODO crée une variable à partir de l'intervale représentant sa valeur
+    	this.name=name;
+    	this.isANumber = false;
+        this.valueInterval = valueInterval;
     }
 
     public Variable(String name, List<Integer> valueSet) {
         //TODO crée une variable à partir de l'ensemble des valeurs qu'elle peut contenir
+    	this.name=name;
+    	this.isANumber = false;
+    	this.valueSet=valueSet;
+    }
+    public Variable(String name, Integer value) {
+        //TODO crée une variable à partir dune constante
+    	this.name=name;
+    	this.isANumber = true;
+    	this.constantValue=value;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
+
