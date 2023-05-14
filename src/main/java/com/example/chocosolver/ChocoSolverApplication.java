030e44b2904b7ca4d13571079305b384d53281a5
Admin <elie.nicolas@etu.univ-amu.fr>
2023-05-14 18:50:12 +0200

    Mon message de commit

--- src/main/java/com/example/chocosolver/ChocoSolverApplication.java
+++ src/main/java/com/example/chocosolver/ChocoSolverApplication.java
@@ -2,12 +2,15 @@ package com.example.chocosolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ChocoSolverApplication {
+public class ChocoSolverApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ChocoSolverApplication.class, args);
     }
 
+   
+    
 }
--- src/test/java/com/example/chocosolver/problem/ChocoProblemTest.java
+++ src/test/java/com/example/chocosolver/problem/ChocoProblemTest.java
@@ -1,7 +1,6 @@
 package com.example.chocosolver.problem;
 
-import org.chocosolver.solver.constraints.Constraint;
-import org.chocosolver.solver.variables.IntVar;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
