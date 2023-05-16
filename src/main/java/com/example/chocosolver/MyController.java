de1b722774b58573b94d4185fd0768019e366c1d
Achraf <achraf.ZERHOUNI-ABDOU@etu.univ-amu.fr>
2023-05-16 11:23:44 +0200

    Page principale: version initiale

--- src/main/java/com/example/chocosolver/MyController.java
+++ src/main/java/com/example/chocosolver/MyController.java
@@ -1,16 +1,13 @@
 package com.example.chocosolver;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 
-    @RequestMapping("/home")
-    public ModelAndView hello() {
-        ModelAndView modelAndView = new ModelAndView("Home");
-        modelAndView.addObject("message", "Hello World!");
-        return modelAndView;
+    @GetMapping("/")
+    public String index() {
+        return "home";
     }
 }
